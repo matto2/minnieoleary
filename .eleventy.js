7530f76ff3a5474fa26fd9ca94d939d82715b653
Matt O'Leary <mattosurf@gmail.com>
2020-10-11 21:37:09 -0700

    try favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,13 @@ module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("assets")
 
+    eleventyConfig.addPassthroughCopy("android-chrome-192x192.png")
     eleventyConfig.addPassthroughCopy("apple-touch-icon.png")
+    eleventyConfig.addPassthroughCopy("favicon-16x16.png")
+    eleventyConfig.addPassthroughCopy("favicon-32x32.png")
+    eleventyConfig.addPassthroughCopy("favicon.ico")
 
-
+    
     return {
       passthroughFileCopy: true,  
       dir: {
